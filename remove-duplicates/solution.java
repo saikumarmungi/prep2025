3475e865b0301cbcbaf02c86f8835714d9bc4c06
Sai Kumar Mungi <saikumarmungi@Sais-MacBook-Air.local>
2025-02-18 12:18:45 +0530

    remove-duplicates-sorted-array

---
+++ remove-duplicates/solution.java
@@ -0,0 +1,29 @@
+package prep2025;/*
+Remove Duplicates from Sorted Array
+
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+*/
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length == 0)
+            return 0;
+
+        int k = 1;
+        for(int i = 1, cur = 0; i < nums.length; i ++){
+
+          if(nums[i] != nums[cur]){
+              nums[k] = nums[i];
+              cur = i;
+              k ++;
+          }
+        }
+
+        return k;
+    }
+}
